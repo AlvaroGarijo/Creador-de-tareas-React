37bc6d2f622b35808885bd3f679d34317b0def39
AlvaroGarijo <agarijo@padelmanager.com>
2022-08-24 10:33:49 +0200

    Logo y Footer hechos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Footer } from './componentes/Footer';
+import freCodeCampLogo from './imagenes/freeCodeCampLogo.png'
 
 function App() {
   return (
-    <div className="App">
-      Hola Mundo
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-container'> 
+        <img
+        className='freecodecamp-logo'
+        src={freCodeCampLogo}
+        alt='logo de freeCodeCamp'/>
+      </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/componentes/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <div className='footer'>Álvaro Garijo © Creador de Tareas 2022</div>
+    </div>
+  )
+}
